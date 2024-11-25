1f103a1e81058482b0b1a02cb8ea5568541abeda
Trayana Boykova <trayana.boykova@gmail.com>
2024-11-25 11:49:08 +0200

    Update functions.php

--- wp-content/themes/softuni/functions.php
+++ wp-content/themes/softuni/functions.php
@@ -7,9 +7,8 @@ add_post_type_support( 'excerpt', array() );
 
 add_action( 'wp_enqueue_scripts', 'softuni_enqueue_assets' );
 function softuni_enqueue_assets() {
-    // wp_enqueue_style( 'softuni', get_stylesheet_directory_uri() . '/style.css');
-    // wp_enqueue_style( 'softuni', get_stylesheet_uri() );
-
+    wp_enqueue_style( 'softuni-main-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0' );
+	// wp_enqueue_script();
 }
 
 
